6727ce5d9360f2d4aec88cc4b85ae11b520e8b02
Daniel Westergren <westis@gmail.com>
2024-03-08 14:25:05 +0100

    Made Race Dashboard the index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,21 @@
 <template>
-  <HelloWorld />
+  <v-container class="pa-5">
+    <v-row justify="center">
+      <v-col cols="12">
+        <race-timer />
+      </v-col>
+      <v-col cols="12">
+        <race-chart />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script lang="ts" setup>
-  //
+<script setup>
+import RaceTimer from "@/components/RaceTimer.vue";
+import RaceChart from "@/components/RaceChart.vue";
 </script>
+
+<style scoped>
+/* Add any page-specific styling here */
+</style>

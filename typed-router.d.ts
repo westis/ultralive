169b1b3e6504b6b3e653b10c1eeba75ac915abd0
Daniel Westergren <westis@gmail.com>
2024-03-08 14:35:41 +0100

    GitHub Pages fixes

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -40,7 +40,6 @@ import type {
 declare module 'vue-router/auto/routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
-    '/RaceDashboard': RouteRecordInfo<'/RaceDashboard', '/RaceDashboard', Record<never, never>, Record<never, never>>,
   }
 }
 

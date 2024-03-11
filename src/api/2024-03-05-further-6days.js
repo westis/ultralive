fabe34e2583666d35dddce7d3d214217dff91994
Daniel Westergren <westis@gmail.com>
2024-03-11 01:24:51 +0100

    Added some other runners

--- src/api/2024-03-05-further-6days.js
+++ src/api/2024-03-05-further-6days.js
@@ -12,7 +12,7 @@ const eventId = "2024-03-05-further-6days";
 
 export async function fetchEventData_20240305_further6days() {
   const response = await fetch(
-    "https://api.rtrt.me/events/TLMR-FURTHER-FEAT-2024/profiles/RB3Y62DA,R6GMUPER,RB7V9PA2/splits?max=10000&appid=65bc00f7eca96e43f970aa14&token=07607b7c6c2409781943c9af7529f020"
+    "https://api.rtrt.me/events/TLMR-FURTHER-FEAT-2024/profiles/RB3Y62DA,R6GMUPER,RB7V9PA2,RH5KWABJ,RM93JEXU,RCYVPFXM/splits?max=10000&appid=65bc00f7eca96e43f970aa14&token=07607b7c6c2409781943c9af7529f020"
   );
   if (!response.ok) throw new Error("Failed to fetch ultramarathon2024 data");
   const jsonData = await response.json();
